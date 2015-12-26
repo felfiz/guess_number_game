568c8437aa23443dc0b761b4df7a34b769d7e59e
Felipe Rodrigues <feliperodrigues@gmail.com>
2015-12-26 00:43:39 -0200

    Change variables name to be more clear

--- guess_number.py
+++ guess_number.py
@@ -1,26 +1,26 @@
 import random
 
 def guess_game():
-  right_number = random.randint(1, 10)
+  secret_number = random.randint(1, 10)
   guesses = []
 
   while len(guesses) < 3:
     print("Guess a number between 1 and 10")
     try:
-      secret_number = int(input("> "))
+      guess_number = int(input("> "))
     except ValueError:
-      print('{} is not a number'.format(secret_number))
+      print('{} is not a number'.format(guess_number))
     else:
-      guesses.append(secret_number)
+      guesses.append(guess_number)
 
-      if secret_number == right_number:
+      if guess_number == secret_number:
         print("You got it :)")
         break
       else:
-        if secret_number < right_number:
-          print('The number is greater than {}'.format(secret_number))
+        if guess_number < secret_number:
+          print('The number is greater than {}'.format(guess_number))
         else:
-          print('The number is lesser than {}'.format(secret_number))
+          print('The number is lesser than {}'.format(guess_number))
   else:
-    print("Sorry you lose :( . My number was {}".format(right_number))
+    print("Sorry you lose :( . My number was {}".format(secret_number))
 guess_game()
