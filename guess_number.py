57c5a5a813848bea08807de222a4a92bac4239d9
Felipe Rodrigues <feliperodrigues@gmail.com>
2015-12-26 00:28:40 -0200

    Turn variable more clear

--- guess_number.py
+++ guess_number.py
@@ -4,12 +4,12 @@ right_number = random.randint(1, 10)
 
 while True:
   print("Guess the number... ")
-  try_number = input("> ")
+  secret_number = input("> ")
 
-  if try_number == right_number:
+  if secret_number == right_number:
     print("You got it :)")
   else:
-    if try_number < right_number:
-      print('The number is greater than {}'.format(try_number))
+    if secret_number < right_number:
+      print('The number is greater than {}'.format(secret_number))
     else:
-      print('The number is lesser than {}'.format(try_number))
+      print('The number is lesser than {}'.format(secret_number))
