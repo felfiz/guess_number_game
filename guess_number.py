54fd2928626f292b2e125f5f98c4234d4f09f582
Felipe Rodrigues <feliperodrigues@gmail.com>
2015-12-26 00:37:02 -0200

    Limit number of tries

--- guess_number.py
+++ guess_number.py
@@ -2,17 +2,24 @@ import random
 
 def guess_game():
   right_number = random.randint(1, 10)
+  guesses = []
 
-  while True:
+  while len(guesses) < 3:
     print("Guess the number... ")
-    secret_number = input("> ")
-
-    if secret_number == right_number:
-      print("You got it :)")
+    try:
+      secret_number = int(input("> "))
+    except ValueError:
+      print('{} is not a number'.format(secret_number))
     else:
-      if secret_number < right_number:
-        print('The number is greater than {}'.format(secret_number))
+      guesses.append(secret_number)
+
+      if secret_number == right_number:
+        print("You got it :)")
+        break
       else:
-        print('The number is lesser than {}'.format(secret_number))
+        if secret_number < right_number:
+          print('The number is greater than {}'.format(secret_number))
+        else:
+          print('The number is lesser than {}'.format(secret_number))
 
 guess_game()
