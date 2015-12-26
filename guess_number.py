c60b5d8c48c845a6816b39ec2707a67a212a1748
Felipe Rodrigues <feliperodrigues@gmail.com>
2015-12-26 00:30:20 -0200

    Add guess game func

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,18 @@
 import random
 
-right_number = random.randint(1, 10)
+def guess_game():
+  right_number = random.randint(1, 10)
 
-while True:
-  print("Guess the number... ")
-  secret_number = input("> ")
+  while True:
+    print("Guess the number... ")
+    secret_number = input("> ")
 
-  if secret_number == right_number:
-    print("You got it :)")
-  else:
-    if secret_number < right_number:
-      print('The number is greater than {}'.format(secret_number))
+    if secret_number == right_number:
+      print("You got it :)")
     else:
-      print('The number is lesser than {}'.format(secret_number))
+      if secret_number < right_number:
+        print('The number is greater than {}'.format(secret_number))
+      else:
+        print('The number is lesser than {}'.format(secret_number))
+
+guess_game()
